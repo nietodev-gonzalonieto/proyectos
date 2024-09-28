90990e4d29bcab66fbb0dd1d9e69b3a10285f64c
Gonzalo Nieto <gonzalonieto@nietodev.com>
2024-09-28 15:55:30 +0200

    Delete personales/web_fisioterapeuta directory

--- personales/web_fisioterapeuta/volverArriba.php
+++
@@ -1,5 +0,0 @@
-<!-- Voler hacía arriba empieza -->
-<div id="back-top" class="d-none d-md-block">
-    <a title="Go to Top" href="#"> <i class="fas fa-level-up-alt"></i></a>
-</div>
-<!-- Volver hacía abajo acaba -->
